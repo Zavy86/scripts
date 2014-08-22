09fa37edba96268a96f90182d2824b34f78b50bf
Manuel Zavatta <manuel.zavatta@gmail.com>
2014-08-22 10:31:58 +0200

    Pull all the specified Git repositories

---
+++ BASH/git_pull.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+# Script: git_pull.sh
+#
+# Description: Pull all the specified Git repositories
+# Author: Manuel Zavatta
+# Last update: 22.08.2014
+#
+
+# htdocs and htuser
+HTDOCS="/var/www/";
+HTUSER="www-data";
+
+# display usage
+display_usage(){
+ printf "\n Zavy's Scripts\n";
+ printf " https://github.com/Zavy86/scripts\n\n"
+ printf " Pull all the specified Git repositories\n"
+ printf " This script require to be *root* to be executed!\n\n"
+ printf " Usage: $0 repository1 repository2 repositoryN\n\n"
+}
+
+# check root
+if [ "$EUID" -ne 0 ]
+then
+ display_usage
+ exit 1
+fi
+
+# check for arguments
+if test -z "$1"
+then
+ display_usage
+ exit 1
+fi
+
+# check for help
+if [[ ("$1" == "-h") || ("$1" == "--help") ]]
+then
+ display_usage
+ exit 0
+fi
+
+# cycle agrument repository array
+for REPOSITORY in "$@"
+do
+ printf "\nPull repository $REPOSITORY\n"
+ su -s /bin/bash $HTUSER -c "cd $HTDOCS$REPOSITORY && git pull"
+done
+printf "\nAll done!\n\n"
+exit 0
