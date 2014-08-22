8fe5d77d8b1a943d3b09681710e70648983f4d75
Manuel Zavatta <manuel.zavatta@gmail.com>
2014-08-22 11:06:49 +0200

    Statistics fix

--- BASH/shell_stats.sh
+++ BASH/shell_stats.sh
@@ -1,37 +1,41 @@
 #!/bin/bash
-#==========================================================================
 #
 # Script: shell_stats.sh
 #
-# Description: Show Host Statistics
+# Description: Show host statistics
 # Author: Manuel Zavatta
-# Last update: 21.08.2014
+# Last update: 22.08.2014
 #
-#==========================================================================
-# HOSTNAME
+
+# get hostname
 HOSTNAME=`hostname -f`
-# IP ADDRESS
+
+# get IP address
 IP=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
-# CPU
-#echo `top -b -n1 | grep "Cpu(s)" | awk '{print $2 + $4}'`
+
+# get cpu usage
 CPU=`grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}'`
 CPU=${CPU%.*}
-# RAM
+
+# get memory usage
 RAM=`free -m | grep Mem`
 RAM_TOTAL=`echo $RAM | cut -f2 -d' '`
 RAM_CURRENT=`echo $RAM | cut -f3 -d' '`
 RAM_PERC=$(echo "scale = 2; $RAM_CURRENT/$RAM_TOTAL*100" | bc)
 RAM=${RAM_PERC%.*}
-# HDD
+
+# get hard disk usage
 HDD=`df -lh | awk '{if ($6 == "/") { print $5 }}' | head -1 | cut -d'%' -f1`
-# UPTIME
+
+# get uptime
 UPTIME=$(</proc/uptime)
 UPTIME=${UPTIME%%.*}
 UPTIME_S=$((UPTIME%60))
 UPTIME_M=$((UPTIME/60%60))
 UPTIME_H=$((UPTIME/60/60%24))
 UPTIME_D=$((UPTIME/60/60/24))
-# SHOW
+
+# show statistics
 echo ""
 echo -e "HOST:\t$HOSTNAME\tIP: $IP"
 echo ""
@@ -39,4 +43,3 @@ echo -e "CPU:\t$CPU % \tRAM: $RAM % \tHDD: $HDD %"
 echo ""
 echo -e "UPTIME:\t$UPTIME_D days, $UPTIME_H hours, $UPTIME_M minutes"
 echo ""
-#==========================================================================
